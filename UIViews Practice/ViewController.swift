2afe7056fa9770c76c2ea35f5ee1bf9bf923cf39
David Siemionko <dsiemionko4406@stu.d214.org>
2020-11-04 14:43:19 -0600

    Initial Commit

---
+++ UIViews Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViews Practice
+//
+//  Created by David Siemionko on 11/4/20.
+//  Copyright © 2020 David Siemionko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
